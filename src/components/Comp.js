9080930a60c02025ff8cf4af7ba3a55219c0ca9a
GOGU EDUKONDAL <97788201+goguedukondal@users.noreply.github.com>
2022-11-10 10:35:47 +0530

    Add files via upload

---
+++ src/components/Comp.js
@@ -0,0 +1,22 @@
+const Comp = ({ colors }) => {
+    console.log(colors);
+    return (
+        <>
+        <h2>Colors & ColorCode</h2>
+        <hr />
+
+            {colors.map((e, idx) => {
+                return (
+                    <div className = "displays" style={{ height:130, width:110}}>
+                        <p style={{height:100, width:110, backgroundColor:`${e.color_code}`}}> </p>
+                        <p className="text" key={idx} >{e.color_code}</p>
+                        <span className="text"  style={{color:`${e.color_code}`, fontSize:10}} >{e.color_name}</span>
+
+                    </div>
+                )
+            })}
+        </>
+    )
+}
+
+export default Comp;
